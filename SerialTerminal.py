29a864f6fcb5311abbef2051d125b6908424926c
Albert <mail@albertskog.se>
2018-10-02 08:27:28 +0200

    Add click events to copy and resend commands

--- SerialTerminal.py
+++ SerialTerminal.py
@@ -13,7 +13,8 @@ class SerialTerminal(tk.LabelFrame):
 
         self.terminal = tk.Text(self,
                                    width=50,
-                                   font=("Consolas", 16))
+                                   font=("Consolas", 16),
+                                   state=tk.DISABLED)
         self.setup_tags()
 
         self.y_scroll = tk.Scrollbar(self,
@@ -38,6 +39,7 @@ class SerialTerminal(tk.LabelFrame):
     def setup_tags(self):
         self.terminal.tag_config("error",
                                  background="#faa",
+                                 border=1,
                                  lmargin1=3,
                                  spacing1=3,
                                  spacing3=3)
@@ -50,13 +52,24 @@ class SerialTerminal(tk.LabelFrame):
                                  lmargin1=3,
                                  spacing1=8,
                                  spacing3=2)
+        self.terminal.tag_bind("command", "<Enter>", self.mouse_enter_command_callback)
+        self.terminal.tag_bind("command", "<Leave>", self.mouse_leave_command_callback)
+        self.terminal.tag_bind("command", "<Button-1>", self.mouse_click_command_callback)
+        self.terminal.tag_bind("command", "<Double-Button-1>", self.mouse_double_click_command_callback)
+        self.terminal.tag_config("command_hover",
+                                 background="#eee",
+                                 lmargin1=3,
+                                 spacing1=8,
+                                 spacing3=2)
         self.terminal.tag_config("response",
                                  lmargin1=8,
                                  spacing1=1,
                                  spacing3=1)
 
     def add_line(self, text, tag="info"):
+        self.terminal.config(state=tk.NORMAL)
         self.terminal.insert(tk.END, "{}\n".format(text), tag)
+        self.terminal.config(state=tk.DISABLED)
         self.terminal.yview_moveto(1)
 
     def send_callback(self, event=None):
@@ -65,3 +78,24 @@ class SerialTerminal(tk.LabelFrame):
             self.command_line.delete(0, tk.END)
         else:
             self.add_line("Not connected", "error")
+
+    def mouse_enter_command_callback(self, event):
+        self.terminal.configure(cursor="hand")
+
+    def mouse_leave_command_callback(self, event):
+        self.terminal.configure(cursor="arrow")
+    
+    def mouse_click_command_callback(self, event):
+        index = event.widget.index("@{},{}".format(event.x, event.y))
+        start = "{} linestart".format(index)
+        end = "{} lineend".format(index)
+        command = event.widget.get(start, end)
+        self.command_line.delete(0, tk.END)
+        self.command_line.insert(0, command)
+
+    def mouse_double_click_command_callback(self, event):
+        index = event.widget.index("@{},{}".format(event.x, event.y))
+        start = "{} linestart".format(index)
+        end = "{} lineend".format(index)
+        command = event.widget.get(start, end)
+        self.master.master.serial.send_data(command)
